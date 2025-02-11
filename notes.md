0d347c33a64e2d445b701c3e1e49ce2ec18dba23
danielgraviet <dgravs5@gmail.com>
2025-02-11 15:09:16 -0700

    updated notes

--- notes.md
+++ notes.md
@@ -15,3 +15,8 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
 ## HTML Notes
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+## React Notes
+
+- useState: like a notepad and pencil. The notepad is the state and holds the current information, the pencil is like the setter function and lets you update what is written on the notepad.
+- props: like a gift box. A parent component wraps data in a gift box (props) and passes the box to a child component. Child component can look inside, use the contnets, but can't change the contents.
--- src/app.jsx
+++ src/app.jsx
@@ -1,60 +1,91 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './app.css';
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
-
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login';
-import { About } from './about/about';
 import { Play } from './play/play';
 import { Scores } from './scores/scores';
+import { About } from './about/about';
+import { AuthState } from './login/authState';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
 
-export default function App() {
-    return (
-        <BrowserRouter>
-            <div className='body bg-dark text-light'>
-                <header className='container-fluid'>
-                    <nav className='navbar fixed-top navbar-dark'>
-                        <div className='navbar-brand'>
-                            Simon<sup>&reg;</sup>
-                        </div>
-                        <menu className='navbar-nav'>
-                            <li className='nav-item'>
-                                <NavLink className='nav-link' to=''>Login</NavLink>
-                            </li>
-                            <li className='nav-item'>
-                                <NavLink className='nav-link' to='play'>Play</NavLink>
-                            </li>
-                            <li className='nav-item'>
-                                <NavLink className='nav-link' to='scores'>Scores</NavLink>
-                            </li>
-                            <li className='nav-item'>
-                                <NavLink className='nav-link' to='about'>About</NavLink>
-                            </li>
-                        </menu>
-                    </nav>
-                </header>
+function App() {
+  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+  const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+  const [authState, setAuthState] = React.useState(currentAuthState);
 
-                <Routes>
-                    <Route path='/' element={<Login />} exact />
-                    <Route path='/play' element={<Play />} />
-                    <Route path='/scores' element={<Scores />} />
-                    <Route path='/about' element={<About />} />
-                    <Route path='*' element={<NotFound />} />
-                </Routes>
-
-                <footer className='bg-dark text-white-50'>
-                    <div className='container-fluid'>
-                        <span className='footer-links'>
-                            <a className='text-reset' href='https://github.com/danielgraviet/simon-html'>GitHub</a>
-                            <a className='text-reset' href='https://github.com/danielgraviet/simon-html'>Source</a>
-                        </span>
-                    </div>
-                </footer>
+  return (
+    <BrowserRouter>
+      <div className='body bg-dark text-light'>
+        <header className='container-fluid'>
+          <nav className='navbar fixed-top navbar-dark'>
+            <div className='navbar-brand'>
+              Simon<sup>&reg;</sup>
             </div>
-        </BrowserRouter>
-    );
+            <menu className='navbar-nav'>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to=''>
+                  Login
+                </NavLink>
+              </li>
+              {authState === AuthState.Authenticated && (
+                <li className='nav-item'>
+                  <NavLink className='nav-link' to='play'>
+                    Play
+                  </NavLink>
+                </li>
+              )}
+              {authState === AuthState.Authenticated && (
+                <li className='nav-item'>
+                  <NavLink className='nav-link' to='scores'>
+                    Scores
+                  </NavLink>
+                </li>
+              )}
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='about'>
+                  About
+                </NavLink>
+              </li>
+            </menu>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Login
+                userName={userName}
+                authState={authState}
+                onAuthChange={(userName, authState) => {
+                  setAuthState(authState);
+                  setUserName(userName);
+                }}
+              />
+            }
+            exact
+          />
+          <Route path='/play' element={<Play userName={userName} />} />
+          <Route path='/scores' element={<Scores />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+
+        <footer className='bg-dark text-dark text-muted'>
+          <div className='container-fluid'>
+            <span className='text-reset'>Author Name(s)</span>
+            <a className='text-reset' href='https://github.com/webprogramming260/simon-react'>
+              Source
+            </a>
+          </div>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 function NotFound() {
-    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
+
+export default App;
